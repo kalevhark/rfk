6b1138c48d5a66f61430323aacfe1774ba97b0f8
kalevhark <Kalev.Hark@mail.ee>
2021-06-13 12:02:47 +0300

    version 2021.06.13

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -7,4 +9,4 @@ urlpatterns = [
     path('get_icf_calcs/', views.get_icf_calcs, name='get_icf_calcs'),
     path('get_icf_path/', views.get_icf_path, name='get_icf_path'),
     path('get_icf_summary/', views.get_icf_summary, name='get_icf_summary')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
