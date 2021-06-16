06b197be161c08797d2e62af621ec02e706d6acf
kalevhark <Kalev.Hark@mail.ee>
2021-06-16 21:52:52 +0300

    version 2021.06.16

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('get_icf_calcs/', views.get_icf_calcs, name='get_icf_calcs'),
     path('get_icf_path/', views.get_icf_path, name='get_icf_path'),
     path('get_icf_summary/', views.get_icf_summary, name='get_icf_summary')
--- rfk/urls.py
+++ rfk/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from main import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
