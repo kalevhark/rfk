b42f59a4105837e2e56c973dc83f12ccc67e4c70
kalevhark <Kalev.Hark@mail.ee>
2021-11-01 16:06:55 +0200

    kysimustik4

--- main/static/main/kysimustik4.js
+++ main/static/main/kysimustik4.js
@@ -5,6 +5,48 @@ Vue.createApp({
       picked: ['','','','','','','','','','','','','','','','','',''],
       show: true,
       selected: '',
+      valdkonnad: [
+        [
+          'Liikumine',
+          [
+            [
+              'Võtmevaldkond 1',
+              [
+                ['Küsimus 1', ''], ['Küsimus 2', ''], ['Küsimus 3', '']
+              ]
+            ],
+            [
+              'Võtmevaldkond 2',
+              [
+                ['Küsimus 1', ''], ['Küsimus 2', '']
+              ]
+            ],
+            [
+              'Võtmevaldkond 3',
+              [
+                ['Küsimus 1', ''], ['Küsimus 2', ''], ['Küsimus 3', '']
+              ]
+            ],
+          ]
+        ],
+        [
+          'Käeline tegevus ja enesehooldus',
+          [
+            [
+              'Võtmevaldkond 1',
+              [
+                ['Küsimus 1', ''], ['Küsimus 2', ''], ['Küsimus 3', '']
+              ]
+            ],
+            [
+              'Võtmevaldkond 2',
+              [
+                ['Küsimus 1', ''], ['Küsimus 2', '']
+              ]
+            ],
+          ],
+        ]
+      ],
       options: [
         { text: '---', value: '' },
         { text: 'Jah', value: 'A' },
@@ -24,6 +66,29 @@ Vue.createApp({
           return ''
         }
         var result = 4/10*this.picked[n]
+        if (result < 2) {
+          return 0
+        }
+        if (result < 2.76) {
+          return 2
+        }
+        if (result < 3.5) {
+          return 3
+        }
+        if (result >= 3.5) {
+          return 4
+        }
+        return ''
+      }
+    },
+    score2_uus(valdkondIndex, v6tmetegevusIndex, kysimusIndex) {
+      // `this` points to the vm instance
+      return function (valdkondIndex, v6tmetegevusIndex, kysimusIndex) {
+        var selected = this.valdkonnad[valdkondIndex][1][v6tmetegevusIndex][1][kysimusIndex][1]
+        if (!selected) {
+          return ''
+        }
+        var result = 4/10*selected
         if (result < 2) {
           return 0
         }
