a67c295564d679265fe97afc8135c4733e3ffc40
kalevhark <Kalev.Hark@mail.ee>
2022-03-20 09:39:17 +0200

    skaalad

--- main/static/main/kysimustik6.js
+++ main/static/main/kysimustik6.js
@@ -2,6 +2,7 @@ Vue.createApp({
   delimiters: ['[[', ']]'],
   data() {
     return {
+      showBackgroundInfo: false,
       vanusgrupid: [
         { kysimustik: false, text: 'LAPS 0-2', value: 0},
         { kysimustik: true, text: 'LAPS 3-8', value: 1},
@@ -9,6 +10,11 @@ Vue.createApp({
         { kysimustik: true, text: 'VPI', value: 3},
       ],
       selectedVanusgrupp: 0,
+      skaalad: [
+        { text: '1-10', skaalaMax: 10, extraKysimus: 4, value: 0},
+        { text: '1-5', skaalaMax: 5, extraKysimus: 2, value: 1},
+      ],
+      selectedSkaala: 0,
       options: [
         { text: '---', value: '' },
         { text: 'Jah', value: 'A' },
@@ -26,19 +32,19 @@ Vue.createApp({
       vanusgruppideKysimused: {
         0: [],
         1: [
-          { text: '3-8 Kysimus', score: '' },
-          { text: 'Kysimus', score: '' },
-          { text: 'Kysimus', score: '' }
+          { text: '3-8 Kysimus', valdkond: '1.', score: '' },
+          { text: 'Kysimus', valdkond: '', score: '' },
+          { text: 'Kysimus', valdkond: '', score: '' }
         ],
         2: [
-          { text: '9-15 Kysimus', score: '' },
-          { text: 'Kysimus', score: '' },
-          { text: 'Kysimus', score: '' }
+          { text: '9-15 Kysimus', valdkond: '1.', score: '' },
+          { text: 'Kysimus', valdkond: '', score: '' },
+          { text: 'Kysimus', valdkond: '', score: '' }
         ],
         3: [
-          { text: 'VPI Küsimus', score: '' },
-          { text: 'Kysimus', score: '' },
-          { text: 'Kysimus', score: '' }
+          { text: 'VPI Küsimus', valdkond: '1.', score: '' },
+          { text: 'Kysimus', valdkond: '', score: '' },
+          { text: 'Kysimus', valdkond: '', score: '' }
         ]
       },
       yldkysimused: [],
@@ -70,7 +76,7 @@ Vue.createApp({
     this.$nextTick(function () {
       // Code that will run only after the
       // entire view has been rendered
-      this.selectedVanusgrupp = 0;
+      this.selectedVanusgrupp = 1;
       this.kysimustik = this.vanusgruppideKysimused[this.selectedVanusgrupp];
       this.yldkysimused = this.vanusgruppideYldKysimused[this.selectedVanusgrupp];
     })
@@ -98,6 +104,33 @@ Vue.createApp({
     },
     showKysimustik() {
       return this.vanusgrupid[this.selectedVanusgrupp].kysimustik && this.toggleShowForm === 'yes';
+    },
+    getScore(kysimusIndex) {
+      // `this` points to the vm instance
+      return function (kysimusIndex) {
+        var selected = this.kysimustik[kysimusIndex]['score'];
+        var skaalaMax = this.skaalad[this.selectedSkaala]['skaalaMax'];
+        if (!selected) {
+          return ''
+        }
+        var result = 4 / skaalaMax * selected
+        if (result < 1) {
+          return 0
+        }
+        if (result < 2) {
+          return 1
+        }
+        if (result < 2.76) {
+          return 2
+        }
+        if (result < 3.5) {
+          return 3
+        }
+        if (result >= 3.5) {
+          return 4
+        }
+        return ''
+      }
     }
   }
 }).mount('#kysimustik6')
