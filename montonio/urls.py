5063a7fc87ddfa223879fef01c1c96b30bcbc057
kalevhark <Kalev.Hark@mail.ee>
2023-04-26 22:55:24 +0300

    montonio

--- montonio/urls.py
+++ montonio/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('naase/', views.naase, name='naase'),
     path('teavita/', views.teavita, name='teavita'),
+    path('get_order/', views.get_order, name='get_order'),
 ]
 
--- montonio/views.py
+++ montonio/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.conf import settings
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
 
@@ -7,7 +10,8 @@ import jwt
 import requests
 import webbrowser
 
-def get_payload():
+def get_payload(user, preferred_region, preferred_provider, amount):
+    exp = int((datetime.utcnow() + timedelta(seconds=10*60)).timestamp())
     # 1. Gather the checkout data
     payload = {
         "accessKey": settings.MY_ACCESS_KEY,
@@ -15,76 +19,92 @@ def get_payload():
         "returnUrl": "http://test.valgalinn.ee:8000/montonio/naase",
         "notificationUrl": "http://test.valgalinn.ee:8000/montonio/teavita",
         "currency": "EUR",
-        "grandTotal": 9.99,
+        "exp": exp,
+        "grandTotal": amount,
         "locale": "et",
-        "billingAddress": {
-            "firstName": "Kalev",
-            "lastName": "Härk",
-            "email": "kalev.hark@mail.ee",
-            "addressLine1": "Kai 1",
-            "locality": "Tallinn",
-            "region": "Harjumaa",
-            "country": "EE",
-            "postalCode": "10111"
-        },
-        "shippingAddress": {
-            "firstName": "Kalev",
-            "lastName": "Härk",
-            "email": "kalev.hark@mail.ee",
-            "addressLine1": "Kai 1",
-            "locality": "Tallinn",
-            "region": "Harjumaa",
-            "country": "EE",
-            "postalCode": "10111"
-        },
-        "lineItems": [
-            {
-                "name": "Annetus",
-                "quantity": 1,
-                "finalPrice": 9.99
-            }
-        ],
+        # "billingAddress": {
+        #     "firstName": "Kalev",
+        #     "lastName": "Härk",
+        #     "email": "kalev.hark@mail.ee",
+        #     "addressLine1": "Kai 1",
+        #     "locality": "Tallinn",
+        #     "region": "Harjumaa",
+        #     "country": "EE",
+        #     "postalCode": "10111"
+        # },
+        # "shippingAddress": {
+        #     "firstName": "Kalev",
+        #     "lastName": "Härk",
+        #     "email": "kalev.hark@mail.ee",
+        #     "addressLine1": "Kai 1",
+        #     "locality": "Tallinn",
+        #     "region": "Harjumaa",
+        #     "country": "EE",
+        #     "postalCode": "10111"
+        # },
+        # "lineItems": [
+        #     {
+        #         "name": "Annetus",
+        #         "quantity": 1,
+        #         "finalPrice": 9.99
+        #     }
+        # ],
 
         # 2. Specify the payment method
         "payment": {
             "method": "paymentInitiation",
             "methodDisplay": "Maksa läbi oma panga",
             "methodOptions": {
-                "paymentDescription": "Annetus",
-                "preferredCountry": "EE",
+                "paymentDescription": f"{user} annetus",
+                "preferredCountry": preferred_region,
                 # This is the code of the bank that the customer chose at checkout.
                 # See the GET /stores/payment-methods endpoint for the list of available banks.
-                "preferredProvider": "LHVBEE22"
+                "preferredProvider": preferred_provider
             },
-            "amount": 9.99,  # Yes, this is the same as order['grandTotal'].
+            "amount": amount,  # Yes, this is the same as order['grandTotal'].
             "currency": "EUR"  # This must match the currency of the order.
         }
     }
     return payload
 
+import json
+def get_order(request):
+    if request:
+        # 1. Gather the checkout data
+        # 2. Specify the payment method
+        data = json.loads(request.body)
+        user = data['User']
+        amount = data['Amount']
+        preferred_region = data['Preferred region']
+        preferred_provider = data['Preferred provider']
+
+        payload = get_payload(user, preferred_region, preferred_provider, amount)
+        # 3. Generate the token
+        token = jwt.encode(payload, settings.MY_SECRET_KEY, algorithm='HS256')
+        print(token)
+
+        # 4. Send the token to the API and get the payment URL
+        response = requests.post('https://sandbox-stargate.montonio.com/api/orders', json={
+            'body': token
+        })
+        data = response.json()
+        # payment_url = data['paymentUrl']
+
+        # 5. Redirect the customer to the checkout page
+        print(data)
+        # webbrowser.open(payment_url)
+    else:
+        data = 'NOK'
+    return JsonResponse({'data': data})
+
 def index(request):
-    # 1. Gather the checkout data
-    # 2. Specify the payment method
-    payload = get_payload()
-    # 3. Generate the token
-    token = jwt.encode(payload, settings.MY_SECRET_KEY, algorithm='HS256')
-    print(settings.MY_SECRET_KEY, settings.MY_ACCESS_KEY, token)
+    return render(
+        request,
+        "montonio/index.html",
+        {'my_access_key': settings.MY_ACCESS_KEY}
+    )
 
-    # 4. Send the token to the API and get the payment URL
-    response = requests.post('https://sandbox-stargate.montonio.com/api/orders', json={
-        'body': token
-    })
-    data = response.json()
-    # payment_url = data['paymentUrl']
-
-    # 5. Redirect the customer to the checkout page
-    print(data)
-    # webbrowser.open(payment_url)
-    return redirect('/')
-    # return render(request, "montonio/index.html", {})
-
-def index_bak(request):
-    from datetime import datetime, timedelta
+def get_payment_methods(request):
     payload = {
         'accessKey': settings.MY_ACCESS_KEY,
         'iat': datetime.utcnow(),
@@ -102,7 +122,7 @@ def index_bak(request):
     )
     data = response.json()
     print(data)
-    return redirect('/')
+    return JsonResponse({'data': data})
 
 def naase(request):
     if request:
@@ -111,5 +131,5 @@ def naase(request):
 
 def teavita(request):
     if request:
-        print(request.META)
+        print(request.body)
     return redirect('/')
