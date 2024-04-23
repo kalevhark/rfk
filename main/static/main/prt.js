5ba2d22eb7cf0c8bdb3611fa29bb273961a588ff
kalevhark <Kalev.Hark@mail.ee>
2024-04-23 15:24:51 +0300

    prt new approach

--- main/static/main/prt.js
+++ main/static/main/prt.js
@@ -6,28 +6,50 @@ function openCity(evt, cityName) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove('w3-red');
+    tablinks[i].classList.remove('w3-border');
   }
   var y = document.getElementsByClassName("code_" + cityName);
   for (i = 0; i < y.length; i++) {
     y[i].style.display = "block";
   }
-  evt.currentTarget.classList.add('w3-red');
+  evt.currentTarget.classList.add('w3-border');
 }
 
 function checkCodes() {
+
+  // kontrollime et d-koodiga koos oleks ka b-koodid
   let decks = document.querySelectorAll(".results_on_deck");
+  let d_level1_results = {};
   decks.forEach(
     (deck) => {
       let code = deck.id.split('_', 1)[0];
+      let code_level1 = code.substring(0, 2);
       let m22raja = document.getElementById(code + "_activities").value;
       if (m22raja !== '---' && deck.hasChildNodes() !== true) {
         document.getElementById(code + "_kategooria_text").classList.add('invalid');
       } else {
         document.getElementById(code + "_kategooria_text").classList.remove('invalid');
+        if (d_level1_results[code_level1] !== undefined && d_level1_results[code_level1] < m22raja) {
+          d_level1_results[code_level1] = m22raja;
+        }
+        if (d_level1_results[code_level1] === undefined && m22raja !== '---') {
+          d_level1_results[code_level1] = m22raja;
+        }
       }
     }
   );
+
+  // v3rvime sakid valdkonna d-koodide k6rgeima raskusastme j2rgi
+  const SCORE_CLASSES = ['w3-pale-green', 'w3-sand', 'w3-pale-yellow', 'w3-pale-red', 'w3-red'];
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove('w3-sand', 'w3-pale-yellow', 'w3-pale-red', 'w3-red');
+  }
+
+  for(var d_level1_code in d_level1_results) {
+    var score = d_level1_results[d_level1_code];
+    document.getElementById("tablink_" + d_level1_code).classList.add(SCORE_CLASSES[parseInt(score)])
+  }
 }
 
 function kontrolli(elementId) {
@@ -103,7 +125,7 @@ createApp({
           setTimeout(function(){
             btn.innerHTML = 'Arvuta';
             btn.classList.remove('w3-pale-green');
-          }, 1000);
+          }, 500);
           if ( response.data ) {
             // console.log(response.data);
             document.getElementById('db_level1_matrix').innerHTML = response.data.db_level1_matrix;
